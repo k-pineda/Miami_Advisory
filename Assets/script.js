7d6ca6951dadd7ab75540790c3030ebffa85cc03
Anthony-D99 <anthonydisaf@yahoo.com>
2023-05-22 16:10:20 -0400

    added text area on html, as well as added if statement in js for progress bar changes

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,7 @@ var currentTime = dayjs().format("MMM DD, YYYY  h:mm:ss A")
 
 $("#current-time").text(currentTime)
 
-var currentWeatherAPI=`https://api.openweathermap.org/data/2.5/weather?id=4164138&appid=3484e08d51e803d19133758ad6e77ac5&units=imperial`
+var currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?id=4164138&appid=3484e08d51e803d19133758ad6e77ac5&units=imperial`
 
 var currentFeelsLikeTemp;
 var cityName;
@@ -14,25 +14,36 @@ var currentWeatherIcon;
 // when current feel is less than 90 move progress bar to 🙂🐕✅ 
 
 
-function getWeatherAPI (currentWeatherAPI){
+function getWeatherAPI(currentWeatherAPI) {
     fetch(currentWeatherAPI)
-        .then(function (response) 
-        {
-          return response.json();
+        .then(function (response) {
+            return response.json();
         })
-        .then(function(data){
+        .then(function (data) {
             console.log(data)
             console.log(data.main.feels_like)
             console.log(data.name)
             console.log(data.weather[0].icon)
-        })
-    }
-    
-function getWeatherIconAPI() 
-{
+            if (data.main.feels_like <= 80) {
+                $('.25').removeClass('hide')
+            } if (data.main.feels_like <= 90) {
+                $('.25').addClass('hide')
+                $('.50').removeClass('hide')
+            }if (data.main.feels_like <= 104) {
+                $('.50').addClass('hide')
+                $('.75').removeClass('hide')
+            }else{
+                $('.75').addClass('hide')
+                $('.100').removeClass('hide')
+            }
+
+            })
+}
+
+function getWeatherIconAPI() {
     // $('img').remove()
-    var currentWeatherIcon=data.weather[0].icon
-    var img = $('<img>', {src:`https://openweathermap.org/img/wn/${currentWeatherIcon}@2x.png`})
+    var currentWeatherIcon = data.weather[0].icon
+    var img = $('<img>', { src: `https://openweathermap.org/img/wn/${currentWeatherIcon}@2x.png` })
     return img
 }
 
